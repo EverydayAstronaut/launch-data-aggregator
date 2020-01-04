1a21771b848b90a60faf59885ac13459ac659c32
Jeffrey Maas <jeffrey.maas@ahold.com>
2020-01-04 02:44:03 +0100

    fixed model issue where spacex recovery info did not show core serial number

--- src/main/kotlin/com/launchAggregator/aggregator/model/RecoveryInfo.kt
+++ src/main/kotlin/com/launchAggregator/aggregator/model/RecoveryInfo.kt
@@ -1,6 +1,7 @@
 package com.launchAggregator.aggregator.model
 
 data class RecoveryInfo(
+        val serial: String = "",
         val attempt: Boolean = false,
         val onShip: Boolean = false,
         val location: List<String> = listOf(),
